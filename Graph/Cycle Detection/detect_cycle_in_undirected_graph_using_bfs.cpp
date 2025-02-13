ed2820b59916b3532530cffec50d4f22140ccccd
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-13 23:53:29 +0600

    bfs structure for cyle detection added

--- Graph/Cycle Detection/detect_cycle_in_undirected_graph_using_bfs.cpp
+++ Graph/Cycle Detection/detect_cycle_in_undirected_graph_using_bfs.cpp
@@ -3,6 +3,30 @@ using namespace std;
 bool vis[105];
 vector<int> adj_list[105];
 
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+
+    while(!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        cout << par << endl;
+
+        for(int child: adj_list[par])
+        {
+            if(!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
 int main()
 {
     int n, e;
@@ -13,5 +37,14 @@ int main()
         cin >> a >> b;
         adj_list[a].push_back(b);
         adj_list[b].push_back(a);
+    }
+
+    memset(vis, false, sizeof(vis));
+    for(int i = 0; i < n; i++)
+    {
+        if(!vis[i])
+        {
+            bfs(i);
+        }
     }
 }
