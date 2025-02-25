7b773149f91bffc650fcee37924fefaa8edbf128
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-25 23:32:26 +0600

    bell-man ford

--- Graph/Bellman-Ford Algorithm/bellman-ford.cpp
+++ Graph/Bellman-Ford Algorithm/bellman-ford.cpp
@@ -17,5 +17,18 @@ int main()
 {
     int n ,e;
     cin >> n >> e;
+    vector<Edge> edge_list;
+    while(e--)
+    {
+        int a, b , c;
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a, b ,c));
+        // edge_list.push_back(Edge(b, a ,c));// undirected only
+    }
+
+    for(auto ed: edge_list)
+    {
+        cout << ed.a << ed.b << ed.c << endl;
+    }
 
 }
