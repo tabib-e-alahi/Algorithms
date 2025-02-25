391c94f49b45dc65cd1ee91c60ca03f2a4d40cc7
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-25 23:22:11 +0600

    bell man ford setup

---
+++ Graph/Bellman-Ford Algorithm/bellman-ford.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+    public:
+        int a, b, c;
+        Edge(int a, int b, int c)
+        {
+            this->a = a;
+            this->b = b;
+            this->c = c;
+        }
+};
+
+int main()
+{
+    int n ,e;
+    cin >> n >> e;
+
+}
